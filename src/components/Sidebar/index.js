b6badc8e65cccdd8bbc8c93413f838c1051f6b1d
Ankita Dhiman <ankita@bhumiitech.com>
2024-12-14 12:58:33 +0530

    added selected chat

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -36,65 +36,68 @@ const SidebarComponent = () => {
 
     return (
         <>
-            <Sidebar className="min-h-screen !bg-gray-900 sidebar hidden md:block" style={{ padding: "0px" }}>
-                <Sidebar.Logo img={logo} className="text-gray-200">
-                    Chatbot
-                </Sidebar.Logo>
-                <Sidebar.Items className="!bg-gray-900">
-                    <Sidebar.ItemGroup className="!bg-gray-900">
-                        <Sidebar.Item onClick={handleNewChat} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={MdLibraryAdd}>
-                            New Chat
-                        </Sidebar.Item>
-                        {chatHistory?.length > 0 && <HR />}
-                        {chatHistory?.map((chat, index) => (
-                            <Sidebar.Item
-                                onClick={() => handleChatSelection(chat.id)}
-                                key={index}
-                                className="text-gray-50 hover:bg-gray-800 font-roboto text-sm flex items-start justify-start truncate chat" href="#" icon={TbHistory}>
-                                {chat.messages?.length > 0 ? capitalizeFirstLetter(chat.messages[0].text) : "New chat"}
+            <div className=" h-fit relative">
+
+                <Sidebar className="min-h-screen !bg-gray-900 sidebar hidden md:block" style={{ padding: "0px" }}>
+                    <Sidebar.Logo img={logo} className="text-gray-200">
+                        Chatbot
+                    </Sidebar.Logo>
+                    <Sidebar.Items className="!bg-gray-900">
+                        <Sidebar.ItemGroup className="!bg-gray-900">
+                            <Sidebar.Item onClick={handleNewChat} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={MdLibraryAdd}>
+                                New Chat
                             </Sidebar.Item>
-                        ))}
-                    </Sidebar.ItemGroup>
-                    <Sidebar.ItemGroup className="!bg-gray-900">
-                        <Sidebar.Item onClick={handleClearHistory} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={BiSolidTrashAlt}>
-                            Clear History
-                        </Sidebar.Item>
-                    </Sidebar.ItemGroup>
-                </Sidebar.Items>
-            </Sidebar>
-
-            <Button className="flex md:hidden items-start" onClick={() => setIsOpen(true)}>
-                <HiMenuAlt2 className="text-lg" />
-            </Button>
-
-            <Drawer open={isOpen} onClose={handleClose} className="bg-gray-900 drawer w-64">
-                <Drawer.Header title="Chatbot" titleIcon={() => <img className="w-8 mr-2" src={logo} />} />
-                <Drawer.Items className="">
-                    <Sidebar className="!bg-gray-900 sidebar w-56 p-0">
-                        <Sidebar.Items className="!bg-gray-900">
-                            <Sidebar.ItemGroup className="!bg-gray-900">
-                                <Sidebar.Item onClick={() => { handleNewChat(); handleClose() }} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={MdLibraryAdd}>
-                                    New Chat
+                            {chatHistory?.length > 0 && <HR />}
+                            {chatHistory?.map((chat, index) => (
+                                <Sidebar.Item
+                                    onClick={() => handleChatSelection(chat.id)}
+                                    key={index}
+                                    className="text-gray-50 hover:bg-gray-800 font-roboto text-sm flex items-start justify-start truncate chat" href="#" icon={TbHistory}>
+                                    {chat.messages?.length > 0 ? capitalizeFirstLetter(chat.messages[0].text) : "New chat"}
                                 </Sidebar.Item>
-                                <HR />
-                                {chatHistory?.map((chat, index) => (
-                                    <Sidebar.Item
-                                        onClick={() => { handleChatSelection(chat.id); handleClose() }}
-                                        key={index}
-                                        className="text-gray-50 hover:bg-gray-800 font-roboto text-sm flex items-start justify-start truncate chat" href="#" icon={TbHistory}>
-                                        {chat.messages?.length > 0 ? capitalizeFirstLetter(chat.messages[0].text) : "New chat"}
+                            ))}
+                        </Sidebar.ItemGroup>
+                        <Sidebar.ItemGroup className="!bg-gray-900">
+                            <Sidebar.Item onClick={handleClearHistory} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={BiSolidTrashAlt}>
+                                Clear History
+                            </Sidebar.Item>
+                        </Sidebar.ItemGroup>
+                    </Sidebar.Items>
+                </Sidebar>
+
+                <Button className="block md:hidden h-fit absolute top-0" onClick={() => setIsOpen(true)}>
+                    <HiMenuAlt2 className="text-lg" />
+                </Button>
+
+                <Drawer open={isOpen} onClose={handleClose} className="bg-gray-900 drawer w-64 fixed md:hidden">
+                    <Drawer.Header title="Chatbot" titleIcon={() => <img className="w-8 mr-2" src={logo} />} />
+                    <Drawer.Items className="">
+                        <Sidebar className="!bg-gray-900 sidebar w-56 p-0">
+                            <Sidebar.Items className="!bg-gray-900">
+                                <Sidebar.ItemGroup className="!bg-gray-900">
+                                    <Sidebar.Item onClick={() => { handleNewChat(); handleClose() }} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={MdLibraryAdd}>
+                                        New Chat
                                     </Sidebar.Item>
-                                ))}
-                            </Sidebar.ItemGroup>
-                            <Sidebar.ItemGroup className="!bg-gray-900">
-                                <Sidebar.Item onClick={() => { handleClearHistory(); handleClose() }} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={BiSolidTrashAlt}>
-                                    Clear History
-                                </Sidebar.Item>
-                            </Sidebar.ItemGroup>
-                        </Sidebar.Items>
-                    </Sidebar>
-                </Drawer.Items>
-            </Drawer>
+                                    <HR />
+                                    {chatHistory?.map((chat, index) => (
+                                        <Sidebar.Item
+                                            onClick={() => { handleChatSelection(chat.id); handleClose() }}
+                                            key={index}
+                                            className="text-gray-50 hover:bg-gray-800 font-roboto text-sm flex items-start justify-start truncate chat" href="#" icon={TbHistory}>
+                                            {chat.messages?.length > 0 ? capitalizeFirstLetter(chat.messages[0].text) : "New chat"}
+                                        </Sidebar.Item>
+                                    ))}
+                                </Sidebar.ItemGroup>
+                                <Sidebar.ItemGroup className="!bg-gray-900">
+                                    <Sidebar.Item onClick={() => { handleClearHistory(); handleClose() }} className="text-gray-50 hover:bg-gray-800 font-roboto text-sm" href="#" icon={BiSolidTrashAlt}>
+                                        Clear History
+                                    </Sidebar.Item>
+                                </Sidebar.ItemGroup>
+                            </Sidebar.Items>
+                        </Sidebar>
+                    </Drawer.Items>
+                </Drawer>
+            </div>
         </>
     );
 }
